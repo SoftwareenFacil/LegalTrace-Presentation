1f201a687142cdecbcfbfa4ec8c85048c4897d6a
PamelaElin <sosapamelaelin@gmail.com>
2024-06-14 17:22:55 -0300

    SearchBar/filterPayments-name&title

--- src/Components/Searchs/SearchBar.jsx
+++ src/Components/Searchs/SearchBar.jsx
@@ -3,39 +3,68 @@ import { Form, FormControl, Button } from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
 import "../../Style/SearchStyle.css";
+import { getClients, getPayments } from "../../Utils/getEntity";
 
-function SearchBar({ getEntity, placeholderText, color, setParams,category }) {
-
+function SearchBar({ getEntity, placeholderText, color, setParams, category }) {
   const [searchText, setSearchText] = useState('');
 
   const handleSearch = async (event) => {
     event.preventDefault();
-  
-    let conditionalsParams;
-  
-    if (category === 'tasks') {
-      conditionalsParams = { id: searchText };
-    } else if (category === 'payments') {
-      conditionalsParams = { id: searchText };
-    } else {
-      conditionalsParams = { name: searchText };
+    if (searchText.trim() === '') {
+      setParams({ id: 0, clientId: 0 });
+      return;
     }
-  
-    const response = await getEntity(conditionalsParams);
-  
-    if (response === null) {
-      setParams({ id: 0 });
+
+    let conditionalsParams = {};
+
+    switch (category) {
+      case 'tasks':
+        conditionalsParams = { id: searchText };
+        break;
+      case 'payments':
+        conditionalsParams = { name: searchText };
+        break;
+      default:
+        conditionalsParams = { name: searchText };
+        break;
+    }
+
+    let response;
+    if (category === 'payments') {
+      try {
+        const clientResponse = await getClients({ name: searchText });
+        if (clientResponse && clientResponse.length > 0) {
+          const clientId = clientResponse[0].id;
+          response = await getPayments({ clientId });
+          setParams({ clientId });
+        } else {
+          const paymentResponse = await getPayments({ title: searchText });
+          if (paymentResponse && paymentResponse.length > 0) {
+            response = paymentResponse;
+            setParams({ title: searchText });
+          } else {
+            setParams({ clientId: 0 });
+            response = null;
+          }
+        }
+      } catch (error) {
+        console.error("Search error:", error);
+        setParams({ clientId: 0 });
+      }
     } else {
-      if (category === 'tasks') {
-        setParams({ id: searchText });
-      } else if (category === 'payments') {
-        setParams({ id: searchText });
-      } else {
-        setParams({ name: searchText });
+      try {
+        response = await getEntity(conditionalsParams);
+        setParams(conditionalsParams);
+      } catch (error) {
+        console.error("Search error:", error);
+        setParams({ id: 0 });
       }
     }
+
+    if (response === null || response.length === 0) {
+      setParams({ id: 0 });
+    }
   };
-  
 
   const handleChange = (event) => {
     setSearchText(event.target.value);
@@ -47,8 +76,7 @@ function SearchBar({ getEntity, placeholderText, color, setParams,category }) {
         <Button
           className="btn btn-outline-secondary border-right-0 border custom-search-button"
           style={{ color, borderColor: color }}
-          onClick={handleSearch}
-          type="button"
+          type="submit"
         >
           <FontAwesomeIcon icon={faSearch} />
         </Button>
@@ -66,3 +94,4 @@ function SearchBar({ getEntity, placeholderText, color, setParams,category }) {
 }
 
 export default SearchBar;
+
