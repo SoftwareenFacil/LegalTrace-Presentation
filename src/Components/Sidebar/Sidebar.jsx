bfee70b7c4a7a6a6c800be43b410c7550767463b
PamelaElin <sosapamelaelin@gmail.com>
2024-06-25 18:21:38 -0300

    jwt

--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState,useEffect } from 'react';
 import { NavLink, useNavigate } from "react-router-dom";
 import { Row, Container, Button } from "react-bootstrap";
 import Cookies from 'js-cookie';
@@ -13,26 +13,50 @@ import '../../Style/Buttons/CrearTareaButton.scss';
 
 
 const Sidebar = () => {
-  const token = Cookies.get("token");
-  let decoded = null;
-  let isAdmin = false;
   const navigate = useNavigate();
-  if (typeof token === 'string') {
-    try {
-      decoded = jwtDecode(token);
-      isAdmin = decoded.SuperAdmin === "True";
-    } catch (error) {
-      console.error('Error decoding token:', error);
-       navigate('/login');
-    }
-  } else {
-    console.error('Invalid token: must be a string');
-     navigate('/login'); 
-  }
-
+  const token = Cookies.get("token");
 
 
   const [show, setShow] = useState(false);
+  const [isAdmin, setIsAdmin] = useState(false);
+
+  // let decoded = null;
+  // let isAdmin = false;
+  // if (typeof token === 'string') {
+  //   try {
+  //     decoded = jwtDecode(token);
+  //     isAdmin = decoded.SuperAdmin === "True";
+  //   } catch (error) {
+  //     console.error('Error decoding token:', error);
+  //      navigate('/login');
+  //   }
+  // } else {
+  //   console.error('Invalid token: must be a string');
+  //    navigate('/login'); 
+  // }
+  useEffect(() => {
+    if (token) {
+      try {
+        const decoded = jwtDecode(token);
+        if (decoded.exp * 1000 < Date.now()) {
+          // Token expirado
+          Cookies.remove("token");
+          navigate('/login');
+        } else {
+          setIsAdmin(decoded.SuperAdmin === "True");
+        }
+      } catch (error) {
+        console.error('Error decoding token:', error);
+        Cookies.remove("token");
+        navigate('/login');
+      }
+    } else {
+      navigate('/login');
+    }
+  }, [token, navigate]);
+
+
+
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
