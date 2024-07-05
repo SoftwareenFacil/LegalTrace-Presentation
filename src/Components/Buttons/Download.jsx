e7d808cab16703fed5ae0f634e4b6990989c9294
PamelaElin <sosapamelaelin@gmail.com>
2024-07-04 21:04:14 -0300

    Download/downloading

--- src/Components/Buttons/Download.jsx
+++ src/Components/Buttons/Download.jsx
@@ -1,14 +1,16 @@
+import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 import paymentService from "../../Service/paymentService";
 
 function Download({ fileLink }) {
-
- 
+  const [downloading, setDownloading] = useState(false);
 
   const handleButtonClick = async () => {
     try {
       if (fileLink) {
-        const {url, fileName } = await paymentService.fechFile(fileLink)
+        setDownloading(true); 
+
+        const { url, fileName } = await paymentService.fechFile(fileLink);
 
         const aTag = document.createElement("a");
         aTag.href = url;
@@ -18,25 +20,27 @@ function Download({ fileLink }) {
         document.body.removeChild(aTag);
 
         window.URL.revokeObjectURL(url);
+
+        setDownloading(false); 
       } else {
         console.error("No se encontró el fileLink necesario para la descarga.");
       }
     } catch (error) {
       console.error("Error al descargar el archivo:", error);
+      setDownloading(false); 
     }
   };
 
   return (
     <div>
-  
       <Button
         variant="primary"
         size="sm"
         className="w-100 ver-button"
         onClick={handleButtonClick}
-        disabled={!fileLink}
+        disabled={!fileLink || downloading} 
       >
-        Descargar
+         Descargar
       </Button>
     </div>
   );
