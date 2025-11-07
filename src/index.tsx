adfeaa52bbb507d4fcaf9385797e8254629aad94
Adriel Barrientos M. <adrielgbm@gmail.com>
2025-11-07 16:05:12 -0300

    refactor: remove unused FontAwesome CSS dependency and adjust import order in index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "bootstrap/scss/bootstrap.scss";
-import "./App.scss";
 import "bootstrap/dist/js/bootstrap.bundle.min";
-import "@fortawesome/fontawesome-free/css/all.min.css";
+import "./App.scss";
 
 const rootElement = document.getElementById("root");
 if (!rootElement) throw new Error("Failed to find the root element");
