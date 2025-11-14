bf7e7fee75dfc4ee0939a24c13b9e0864b0a3c68
Adriel Barrientos M. <adrielgbm@gmail.com>
2025-11-14 16:07:22 -0300

    style: update layout and sidebar styles for improved responsiveness and structure

--- src/Components/Layout/Layout.jsx
+++ src/Components/Layout/Layout.jsx
@@ -10,13 +10,9 @@ const Layout = ({ children, setIsAuthenticated }) => {
   return (
     <>
       <NavbarHeader setIsAuthenticated={setIsAuthenticated} />
-      <div className="container-fluid main-content">
-        <div className="row">
-          <div className="col-2 sidebar-container">
-            <Sidebar />
-          </div>
-          <div className="col-10 content-container">{children}</div>
-        </div>
+      <div className="main-content">
+        <Sidebar />
+        <div className="content-container">{children}</div>
       </div>
     </>
   );
